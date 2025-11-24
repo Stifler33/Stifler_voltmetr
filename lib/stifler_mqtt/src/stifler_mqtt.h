1a9747575a176e315da5848f3b60a2d3dace8cd2
Stifler33 <bagi33.ru@gmail.com>
2025-11-24 12:31:14 +0300

    добавил описание некоторых функций

--- lib/stifler_mqtt/src/stifler_mqtt.h
+++ lib/stifler_mqtt/src/stifler_mqtt.h
@@ -9,9 +9,28 @@
 #define NAME_CONFIG_MQTT "config_mqtt"
 extern std::map<String, const char*> pub_topics;
 extern std::map<String, const char*> sub_topics;
+
+/**
+ * сохраняем топик для отправки в него данных
+ * @param name_topic короткое имя топика
+ * @param topic полное имя топика
+ */
 void add_pub_topic(String name_topic, const char* topic);
+
+/**
+ * сохраняем топики из которых мы получаем разные данные или команды
+ * @param name_toic короткое имя топика
+ * @param topic полное имя топика 
+ * @param handler функция формата void(String) которая выполниется при поступлении данных в этот топик
+ */
 void add_sub_topic(String name_topic, const char* topic, std::function<void(String)> handler=nullptr);
 // void add_handler(const char* topic, std::function<void()> handler);
 void init_brocker();
 void loop_mqtt();
+
+/**
+ * Отправляем данные на сервер по имени топика который сохранили функцией add_pub_topic
+ * @param name_topic имя топика
+ * @param payload сообщение в текстовом формате
+ */
 void public_data(String name_topic, const char* payload);
